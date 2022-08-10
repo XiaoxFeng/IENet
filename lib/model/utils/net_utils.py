c191e41ee8f7e9eaff4703e950f9dea9923eeeb7
XiaoxFeng <40047567+XiaoxFeng@users.noreply.github.com>
2022-08-10 23:05:55 +0800

    Add files via upload

---
+++ lib/model/utils/net_utils.py
@@ -0,0 +1,144 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+import numpy as np
+import torchvision.models as models
+from core.config import cfg
+import cv2
+import pdb
+import random
+
+def save_net(fname, net):
+    import h5py
+    h5f = h5py.File(fname, mode='w')
+    for k, v in net.state_dict().items():
+        h5f.create_dataset(k, data=v.cpu().numpy())
+
+def load_net(fname, net):
+    import h5py
+    h5f = h5py.File(fname, mode='r')
+    for k, v in net.state_dict().items():
+        param = torch.from_numpy(np.asarray(h5f[k]))
+        v.copy_(param)
+
+def weights_normal_init(model, dev=0.01):
+    if isinstance(model, list):
+        for m in model:
+            weights_normal_init(m, dev)
+    else:
+        for m in model.modules():
+            if isinstance(m, nn.Conv2d):
+                m.weight.data.normal_(0.0, dev)
+            elif isinstance(m, nn.Linear):
+                m.weight.data.normal_(0.0, dev)
+
+
+def _crop_pool_layer(bottom, rois, max_pool=True):
+    # code modified from
+    # https://github.com/ruotianluo/pytorch-faster-rcnn
+    # implement it using stn
+    # box to affine
+    # input (x1,y1,x2,y2)
+    """
+    [  x2-x1             x1 + x2 - W + 1  ]
+    [  -----      0      ---------------  ]
+    [  W - 1                  W - 1       ]
+    [                                     ]
+    [           y2-y1    y1 + y2 - H + 1  ]
+    [    0      -----    ---------------  ]
+    [           H - 1         H - 1      ]
+    """
+    rois = rois.detach()
+    batch_size = bottom.size(0)
+    D = bottom.size(1)
+    H = bottom.size(2)
+    W = bottom.size(3)
+    roi_per_batch = rois.size(0) / batch_size
+    x1 = rois[:, 1::4] / 16.0
+    y1 = rois[:, 2::4] / 16.0
+    x2 = rois[:, 3::4] / 16.0
+    y2 = rois[:, 4::4] / 16.0
+
+    height = bottom.size(2)
+    width = bottom.size(3)
+
+    # affine theta
+    zero = Variable(rois.data.new(rois.size(0), 1).zero_())
+    theta = torch.cat([\
+      (x2 - x1) / (width - 1),
+      zero,
+      (x1 + x2 - width + 1) / (width - 1),
+      zero,
+      (y2 - y1) / (height - 1),
+      (y1 + y2 - height + 1) / (height - 1)], 1).view(-1, 2, 3)
+
+    if max_pool:
+      pre_pool_size = cfg.POOLING_SIZE * 2
+      grid = F.affine_grid(theta, torch.Size((rois.size(0), 1, pre_pool_size, pre_pool_size)))
+      bottom = bottom.view(1, batch_size, D, H, W).contiguous().expand(roi_per_batch, batch_size, D, H, W)\
+                                                                .contiguous().view(-1, D, H, W)
+      crops = F.grid_sample(bottom, grid)
+      crops = F.max_pool2d(crops, 2, 2)
+    else:
+      grid = F.affine_grid(theta, torch.Size((rois.size(0), 1, cfg.POOLING_SIZE, cfg.POOLING_SIZE)))
+      bottom = bottom.view(1, batch_size, D, H, W).contiguous().expand(roi_per_batch, batch_size, D, H, W)\
+                                                                .contiguous().view(-1, D, H, W)
+      crops = F.grid_sample(bottom, grid)
+
+    return crops, grid
+
+def _affine_grid_gen(rois, input_size, grid_size):
+
+    rois = rois.detach()
+    x1 = rois[:, 1::4] / 16.0
+    y1 = rois[:, 2::4] / 16.0
+    x2 = rois[:, 3::4] / 16.0
+    y2 = rois[:, 4::4] / 16.0
+
+    height = input_size[0]
+    width = input_size[1]
+
+    zero = Variable(rois.data.new(rois.size(0), 1).zero_())
+    theta = torch.cat([\
+      (x2 - x1) / (width - 1),
+      zero,
+      (x1 + x2 - width + 1) / (width - 1),
+      zero,
+      (y2 - y1) / (height - 1),
+      (y1 + y2 - height + 1) / (height - 1)], 1).view(-1, 2, 3)
+
+    grid = F.affine_grid(theta, torch.Size((rois.size(0), 1, grid_size, grid_size)))
+
+    return grid
+
+def _affine_theta(rois, input_size):
+
+    rois = rois.detach()
+    x1 = rois[:, 1::4] / 16.0
+    y1 = rois[:, 2::4] / 16.0
+    x2 = rois[:, 3::4] / 16.0
+    y2 = rois[:, 4::4] / 16.0
+
+    height = input_size[0]
+    width = input_size[1]
+
+    zero = Variable(rois.data.new(rois.size(0), 1).zero_())
+
+    # theta = torch.cat([\
+    #   (x2 - x1) / (width - 1),
+    #   zero,
+    #   (x1 + x2 - width + 1) / (width - 1),
+    #   zero,
+    #   (y2 - y1) / (height - 1),
+    #   (y1 + y2 - height + 1) / (height - 1)], 1).view(-1, 2, 3)
+
+    theta = torch.cat([\
+      (y2 - y1) / (height - 1),
+      zero,
+      (y1 + y2 - height + 1) / (height - 1),
+      zero,
+      (x2 - x1) / (width - 1),
+      (x1 + x2 - width + 1) / (width - 1)], 1).view(-1, 2, 3)
+
+    return theta
